3eaeb68d23e4c9b45f5b7bbfb2199154c545a1e6
Brent Seaton <brentaseaton@gmail.com>
2019-02-21 16:49:44 -0500

    lesson 27 complete

---
+++ src/views/Projects.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="projects">
+    <h1 class="subheading grey--text">Projects</h1>
+
+    <v-container class="my-3">
+      <v-expansion-panel>
+        <v-expansion-panel-content v-for="(project, index) in myProjects" :key="index">
+          <div slot="header">{{ project.title }}</div>
+          <v-card>
+            <v-card-text class="px-4 grey--text">
+              <div class="font-weight-bold">due by {{ project.due }}</div>
+              <div>{{ project.content }}</div>
+            </v-card-text>
+          </v-card>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        projects: [
+          { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iusto nisi porro vitae? Perferendis, fugit rerum impedit id nulla modi sequi architecto amet ea blanditiis aut officia alias. Amet, reiciendis.'},
+          { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iusto nisi porro vitae? Perferendis, fugit rerum impedit id nulla modi sequi architecto amet ea blanditiis aut officia alias. Amet, reiciendis.' },
+          { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iusto nisi porro vitae? Perferendis, fugit rerum impedit id nulla modi sequi architecto amet ea blanditiis aut officia alias. Amet, reiciendis.' },
+          { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iusto nisi porro vitae? Perferendis, fugit rerum impedit id nulla modi sequi architecto amet ea blanditiis aut officia alias. Amet, reiciendis.' },
+          { title: 'Design a new website...again', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iusto nisi porro vitae? Perferendis, fugit rerum impedit id nulla modi sequi architecto amet ea blanditiis aut officia alias. Amet, reiciendis.'}
+        ]
+      }
+    },
+    computed: {
+      myProjects() {
+        return this.projects.filter(project => {
+          return project.person === 'The Net Ninja'
+        })
+      }
+    }
+  }
+</script>
